3ac659cd5a715d524fe3c76dc6b156242e3c7b79
Toinoufu <antoine.cabrera.turzo@gmail.com>
2024-10-04 15:58:23 +0200

    commit temporaire début de qu4

--- tests/test_db.py
+++ tests/test_db.py
@@ -24,3 +24,35 @@ def test_disciplines():
 def test_disciplines_id():
     rows = db.get_disciplines(20)
     assert len(rows) == 1
+
+def test_teams():
+    rows = db.get_teams()
+    assert len(rows) > 1500
+
+def test_teams_id():
+    rows = db.get_teams(150)
+    assert len(rows) == 1
+
+def test_events():
+    rows = db.get_events()
+    assert len(rows) > 250
+
+def test_events_id():
+    rows = db.get_events(150)
+    assert len(rows) == 1
+
+def test_medals():
+    rows = db.get_medals()
+    assert len(rows) > 50
+
+def test_medals_id():
+    rows = db.get_medals(21)
+    assert len(rows) == 1
+
+def test_discipline_athletes():
+    rows = db.get_discipline_athletes()
+    assert len(rows) > 8
+
+def test_discipline_athletes_id():
+    rows = db.get_discipline_athletes(5)
+    assert len(rows) == 1
